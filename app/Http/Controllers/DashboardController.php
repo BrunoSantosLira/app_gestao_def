513066ee93a4dfd7d167b4bb7e88c1e095175c61
brunoSantosLira <brunomasterchif@gmail.com>
2023-12-25 16:02:15 -0300

    commit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -43,9 +43,10 @@ class DashboardController extends Controller
         $clientes = Clientes::all();
         $produtos =  Produtos::with('categoria')->with('unidade')->get();
         $contaCapital = Conta::all();
+        $capitalTotal = $contaCapital[0]['capital'];
         
         $quantidadeClientes = Clientes::count();
         $quantidadeProdutos = Produtos::count();
-        return view('dashboard.index',['quantidadeClientes' => $quantidadeClientes, 'quantidadeProdutos' => $quantidadeProdutos, 'clientes' => $clientes, 'produtos' => $produtos, 'Valorvendas' => $Valorvendas]);
+        return view('dashboard.index',['quantidadeClientes' => $quantidadeClientes, 'quantidadeProdutos' => $quantidadeProdutos, 'clientes' => $clientes, 'produtos' => $produtos, 'Valorvendas' => $Valorvendas,  'contaCapital' => $capitalTotal]);
     }
 }
