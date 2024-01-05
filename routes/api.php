6fdb10a470a3c688a161d9a2a677e8f9a548863e
brunoSantosLira <brunomasterchif@gmail.com>
2024-01-05 15:54:14 -0300

    commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\DashboardController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('http://104.131.166.251/app_gestao_def/public/index.php/soma-vendas-por-dia', [\App\Http\Controllers\DashboardController::class, 'somaVendasPorDia'])->name('/soma-vendas-por-dia');
